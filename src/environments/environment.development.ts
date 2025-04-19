3cb3760a1e4b7f2789e64aee4d08e6ab6c3501e8
sergip8 <sagp758@gmail.com>
2025-04-18 21:16:39 -0500

    add vercel json

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
+    production: false,
     ANGULAR_APP_API_URL: 'http://localhost:7071/api/', // URL de la API
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
+    production: true,
     ANGULAR_APP_API_URL: "https://bookstore-test.azurewebsites.net/api/"
 };
